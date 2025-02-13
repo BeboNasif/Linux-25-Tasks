9b66f327f2a80c729dd29e3bcc7b18da3aeefa0a
BeboNasif <bebo.6ed@gmail.com>
2025-02-13 21:21:25 +0200

    Added Task 7 solution

--- Task_7/commands.sh
+++ Task_7/commands.sh
@@ -2,6 +2,6 @@
 cut -f1 keys >> ttt && cut -f2 keys >> ttt && sort ttt | uniq -d > ~/Desktop/Linux-25-Tasks/Task_7/extracted_keys.txt
 #Part 2
 grep -o '\(https\?://\|www\.\)[^ )!]\+' data.txt
-# To remove dots at the end, we can use (| sed 's/\.$//').
+# To remove dots at the end, we can use (| sed 's/\.$//') I looked it up.
 grep -o '+\?[[:digit:]]\{9,15\}' data.txt
 grep -o '\([[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}\?\)\{3\}' data.txt

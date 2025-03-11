04d715426e47892c18c9c5dec44b2fc23e61c0bd
BeboNasif <bebo.6ed@gmail.com>
2025-03-11 23:06:51 +0200

    Added Script 1

---
+++ Task_9/user_profile.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Enter Your Name"
+read name
+echo "Enter Your Age"
+read age
+echo "Enter Your Favorite Programming Language"
+read pl
+echo -e "Thank you, $name! Your profile has been created successfully with the following details:\n- Name: $name \n- Age: $age \n- Favorite Programming Language: $pl"
